dacff26d69d2fc50f17975ec2bfb9b0b7f45cb55
euzivamjunior <euzivamjunior@hotmail.com>
2020-11-05 10:50:14 -0300

    change jenkisfile to pipeline2

--- pipeline/pipeline2.groovy
+++ pipeline/pipeline2.groovy
@@ -40,7 +40,7 @@ if (params.Continuar != 'Sim') {
                 currentBuild.result = 'FAILURE'
             }
 			archiveArtifacts '*.txt'
-			office365ConnectorSend message: 'Execucao Concluida.', status: 'End', webhookUrl: 'https://outlook.office.com/webhook/f1d4a645-1793-4be1-88e9-ccd82e5c37c8@93f33571-550f-43cf-b09f-cd331338d086/JenkinsCI/f4ab83d5fb7842b59f6b59fd8c0d1c0b/f1d4a645-1793-4be1-88e9-ccd82e5c37c8'
+			office365ConnectorSend message: 'Execucao Concluida.', status: 'End', webhookUrl: 'https://outlook.office.com/webhook/0b7a3292-79a3-4920-bc71-47fcd4555102@93f33571-550f-43cf-b09f-cd331338d086/JenkinsCI/2b011cf39b63478f86dd0b0791675688/0b7a3292-79a3-4920-bc71-47fcd4555102'
         }
 	}
 }
